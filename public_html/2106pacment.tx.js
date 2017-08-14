32d5bf34309e808f628df436568b2cf0afac522c
dimshik100 <dimshik100@gmail.com>
2017-08-14 09:37:49 +0300

    Deleted old files

--- public_html/2106pacment.tx.js
+++
@@ -1,7 +0,0 @@
-
-function draw(br, parentDiv){
-   var div = document.createElement("div");
-   div.style.height = (br.point3.y - br.point1.y) + 'px';
-   div.style.width = (br.point2.x - br.point1.x) + 'px';
-   parentDiv.appendChild(div); 
-}
